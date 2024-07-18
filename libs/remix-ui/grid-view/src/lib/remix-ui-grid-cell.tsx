7a6d95da2983d2930f315bd5e1b4801c571b0c44
lianahus <liana@ethereum.org>
2024-07-18 13:25:12 +0200

    linting

--- libs/remix-ui/grid-view/src/lib/remix-ui-grid-cell.tsx
+++ libs/remix-ui/grid-view/src/lib/remix-ui-grid-cell.tsx
@@ -39,10 +39,10 @@ export const RemixUIGridCell = (props: RemixUIGridCellProps) => {
     if (props.tagList) setAnyEnabled(props.tagList.some((key) => filterCon.keyValueMap[key]?.enabled))
     else setAnyEnabled(filterCon?.keyValueMap['no tag']?.enabled)
     if (!props.tagList || props.tagList.length == 0) setAnyEnabled(true)
-   
+
     setAnyEnabled(
       anyEnabled && (props.title?.toLowerCase().includes(filterCon.filter?.toLowerCase()) || props.searchKeywords?.map(keyword => keyword.toLowerCase()).some(searchKeyword => searchKeyword.toLowerCase().includes(filterCon.filter))))
-    }, [filterCon, props.tagList])
+  }, [filterCon, props.tagList])
 
   /*const listenOnExpand = (key) => {
     if (key === props.key) setExpand(props.toggleExpandView)
